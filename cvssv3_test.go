79ce3fdeaf9636b89705fbe2665ea2dc2eae28d2
Bunji Square <bunji2@users.noreply.github.com>
2019-12-08 09:59:05 +0900

    fix circular dependency

--- cvssv3_test.go
+++ cvssv3_test.go
@@ -1,7 +1,7 @@
 package cvssv3
 
 import (
-	"cvssv3"
+	//"cvssv3"
 	"fmt"
 	"testing"
 )
@@ -53,7 +53,8 @@ func TestBaseScore(t *testing.T) {
 		"CVSS:3.0/AV:N/AC:L/PR:N/UI:R/S:C/C:H/I:H/A:H": 9.6,
 	}
 	for vs,expected := range d {
-		m, err := cvssv3.ParseVector(vs)
+		//m, err := cvssv3.ParseVector(vs)
+		m, err := ParseVector(vs)
 		if err == nil {
 			actual := m.BaseScore()
 			fmt.Printf("%s, expected = %4.1f, actual = %4.1f\n", vs, expected, actual)
